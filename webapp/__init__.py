ac34b70d668cd9c47cc4e100ed3d26d8c76f4c73
Meta (Kraken | X230T) <crabiter@vivaldi.net>
2019-05-12 16:47:11 +0300

    Add Bootstrap-Flask, style login properly

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 dashboard = Admin(app, name='Dashboard')
+bootstrap = Bootstrap(app)
 
 
 from webapp import routes, models, admin
